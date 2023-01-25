f416b5ff20fa6aa6e0d0dff892d0d714e2bf1d01
Metin-3 <7kp0v0e@code.edu.az>
2023-01-25 18:35:55 +0400

    create-project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDoApp from '../components/ToDoApp'
+
+function Home() {
+    return (
+        <>
+            <ToDoApp />
+        </>
+    )
+}
+
+export default Home
